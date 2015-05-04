103d5d9dc68dfcee9cca2163182a872bf86137d3
MrMcCartney <reed.mccartney@cox.net>
2015-05-04 12:15:59 -0500

    Application Start
    Beginning Application

---
+++ src/main/java/com/securitycompany/cardswipe/LogReaderApplication.java
@@ -0,0 +1,29 @@
+package com.securitycompany.cardswipe;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class LogReaderApplication extends Application<LogReaderConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new LogReaderApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "LogReader";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<LogReaderConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final LogReaderConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/com/securitycompany/cardswipe/LogReaderConfiguration.java
@@ -0,0 +1,10 @@
+package com.securitycompany.cardswipe;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class LogReaderConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
